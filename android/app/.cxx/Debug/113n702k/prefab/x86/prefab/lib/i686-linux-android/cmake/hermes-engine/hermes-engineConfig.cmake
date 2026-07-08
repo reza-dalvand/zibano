8a2531f1549dc08982e3d7572514ec5afaedc93a
Reza D <rdalvand@yahoo.com>
2026-07-08 11:15:52 +0330

    Fix .gitignore

--- android/app/.cxx/Debug/113n702k/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/home/reza/.gradle/caches/9.0.0/transforms/f86d68645b029fc0a565608757f94458/transformed/hermes-android-0.14.1-debug/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/reza/.gradle/caches/9.0.0/transforms/f86d68645b029fc0a565608757f94458/transformed/hermes-android-0.14.1-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/113n702k/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-nitro-modules/react-native-nitro-modulesConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-nitro-modules::NitroModules)
-add_library(react-native-nitro-modules::NitroModules SHARED IMPORTED)
-set_target_properties(react-native-nitro-modules::NitroModules PROPERTIES
-    IMPORTED_LOCATION "/home/reza/Documents/zibano/node_modules/react-native-nitro-modules/android/build/intermediates/cxx/Debug/c4r6j1z5/obj/x86/libNitroModules.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/reza/Documents/zibano/node_modules/react-native-nitro-modules/android/build/headers/nitromodules"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
